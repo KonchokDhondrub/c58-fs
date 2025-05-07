9b9ce3f344d452e0bbff2534734f106ebf858439
kd <erm.melnik@gmail.com>
2025-05-07 23:38:41 +0200

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-// import React, { Suspense } from "react";
+import React, { Suspense } from "react";
 import { useState, useEffect } from "react";
 import "./App.css";
 
-// const type = "ls"; // "hw" или "ls"
-// const number = 4;
+const type = "hw"; // "hw" или "ls"
+const number = 4;
 
-// function getSelectedPath(type, num) {
-//   const numCorr = num > 0 && num < 10 ? `0${num}` : `${num}`;
-//   const typeCorr = type === "ls" ? "Lesson" : type === "hw" ? "Homework" : null;
-//   const pathCorr = type === "ls" ? `lessons/lesson_${numCorr}` : type === "hw" ? `hw/hw_${numCorr}` : null;
+function getSelectedPath(type, num) {
+  const numCorr = num > 0 && num < 10 ? `0${num}` : `${num}`;
+  const typeCorr = type === "ls" ? "Lesson" : type === "hw" ? "Homework" : null;
+  const pathCorr = type === "ls" ? `lessons/lesson_${numCorr}` : type === "hw" ? `hw/hw_${numCorr}` : null;
 
-//   return `./${pathCorr}/${typeCorr}${numCorr}.jsx`;
-// }
+  return `./${pathCorr}/${typeCorr}${numCorr}.jsx`;
+}
 
-// const Def = React.lazy(() => import(`${getSelectedPath(type, number)}`));
+const Def = React.lazy(() => import(`${getSelectedPath(type, number)}`));
 
-import Def from "./hw/hw_04/Homework04.jsx";
+// import Def from "./hw/hw_04/Homework04.jsx";
 // import Def from "./lessons/lesson_04/Lesson04.jsx";
 
 function App() {
